85a6dfd04261e937b32d9d4ab1b8b5e48f493adc
adityapadaria <adityapadaria@gmail.com>
2026-03-07 00:19:21 +0530

    Initial commit
    Changes to be committed:
    	new file:   .vscode/settings.json
    	new file:   .vscode/settings.json:Zone.Identifier
    	new file:   .vscode/tasks.json
    	new file:   .vscode/tasks.json:Zone.Identifier
    	new file:   README
    	new file:   cnc.ini
    	new file:   hallib
    	new file:   on_abort.ngc
    	new file:   qtdragon.log
    	new file:   qtdragon.pref
    	new file:   qtdragon.var
    	new file:   qtdragon.var.bak
    	new file:   qtdragon_postgui.hal
    	new file:   qtvcp/screens/qtdragon/__pycache__/resources.cpython-311.pyc
    	new file:   qtvcp/screens/qtdragon/resources.py
    	new file:   silver_dragon.png
    	new file:   start.sh
    	new file:   start.sh:Zone.Identifier
    	new file:   tool.tbl

---
+++ start.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+date=$(date '+%d/%m/%Y %H:%M:%S');
+
+echo "---------------------------------------------------------------------------------------------------------------------------------"
+echo "  Bharat CNC Controller: $date"
+echo "---------------------------------------------------------------------------------------------------------------------------------"
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+# echo " >  Stopping EtherCAT master"
+
+# if lsmod | grep -wq ec_genet; then
+#   sudo rmmod ec_genet
+# fi
+
+# if lsmod | grep -wq ec_generic; then
+#   sudo rmmod ec_generic
+# fi
+
+# if lsmod | grep -wq ec_master; then
+#   sudo /etc/init.d/ethercat stop
+# fi
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+# echo " >  Loading EtherCAT modules"
+
+# sudo modprobe ec_master main_devices=2c:cf:67:8e:ad:a5
+# sudo modprobe ec_generic
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+# echo " >  Binding Ethernet driver to EtherCAT master"
+
+# echo fd580000.ethernet > sudo /sys/bus/platform/drivers/bcmgenet/unbind
+# echo fd580000.ethernet > sudo /sys/bus/platform/drivers/ec_bcmgenet/bind
+
+# sudo chmod 666 /dev/EtherCAT0
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+echo " >  Starting LinuxCNC with EtherCAT configuration"
+echo " "
+
+# sleep 2
+
+# export DISPLAY=:0
+linuxcnc /home/cnc/linuxcnc/configs/lcec-qtvcp/cnc.ini
+
+echo " "
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+# echo " >  Stopping EtherCAT master"
+
+# if lsmod | grep -wq ec_generic; then
+#   sudo rmmod ec_generic
+# fi
+
+# if lsmod | grep -wq ec_master; then
+#   sudo /etc/init.d/ethercat stop
+# fi
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+echo " >  Exit"
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+# sudo reboot
