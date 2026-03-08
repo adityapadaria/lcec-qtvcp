0eb7bed50eac4c46d15e73ba7546f7f4a4f8d338
adityapadaria <adityapadaria@gmail.com>
2026-03-08 17:33:20 +0530

    Basic CNC Layout and Functionality
    Features:
    - Real-time status updates for program running, paused, and idle states.
    - Dynamic enabling/disabling of tabs based on the machine's state (running, paused, idle).
    - Gcode file management and editing capabilities.
    - Customizable UI themes with support for multiple stylesheets.
    - Simulation mode for testing without actual hardware.
    
    Changes to be committed:
    	modified:   .vscode/tasks.json
    	modified:   config.ini
    	modified:   debug.log
    	modified:   gcode/GEORGE-48.ngc
    	new file:   hardware/simulation_core.hal
    	new file:   hardware/simulation_home.hal
    	renamed:    hardware/postgui.hal -> hardware/simulation_postgui.hal
    	new file:   mdi_history.dat
    	modified:   parameters.var
    	modified:   parameters.var.bak
    	modified:   software/__pycache__/software_handler.cpython-313.pyc
    	new file:   software/qtdragon.ui
    	modified:   software/software.pref
    	new file:   software/software.qss
    	modified:   software/software.ui
    	modified:   software/software_handler.py
    	new file:   software/style/argentium.qss
    	new file:   software/style/carbonite.qss
    	new file:   software/style/metal.qss
    	new file:   software/style/obsidian.qss
    	new file:   software/style/olivine.qss

--- software/software_handler.py
+++ software/software_handler.py
@@ -8,13 +8,22 @@ import linuxcnc
 from PyQt5 import QtCore, QtWidgets
 
 from qtvcp.widgets.mdi_line import MDILine as MDI_WIDGET
+from qtvcp.widgets.mdi_history import MDIHistory as MDI_HISTORY
 from qtvcp.widgets.gcode_editor import GcodeEditor as GCODE
+
 from qtvcp.lib.keybindings import Keylookup
 from qtvcp.core import Status, Action
 
 # Set up logging
 from qtvcp import logger
 LOG = logger.getLogger(__name__)
+
+# Mode mapping
+MODE_MAP = {
+    linuxcnc.MODE_MANUAL: "JOG",
+    linuxcnc.MODE_AUTO: "AUTO",
+    linuxcnc.MODE_MDI: "MDI"
+}
 
 # Set the log level for this module
 #LOG.setLevel(logger.INFO) # One of DEBUG, INFO, WARNING, ERROR, CRITICAL
@@ -26,6 +35,10 @@ LOG = logger.getLogger(__name__)
 KEYBIND = Keylookup()
 STATUS = Status()
 ACTION = Action()
+
+stat = linuxcnc.stat()
+comm = linuxcnc.command()
+
 ###################################
 # **** HANDLER CLASS SECTION **** #
 ###################################
@@ -51,7 +64,28 @@ class HandlerClass:
     # the HAL pins are built but HAL is not set ready
     # This is where you make HAL pins or initialize state of widgets etc
     def initialized__(self):
-        pass
+
+        comm.state(linuxcnc.STATE_ESTOP_RESET)
+        comm.wait_complete(10)
+
+        comm.state(linuxcnc.STATE_ON)               # Set initial state to IDLE
+        comm.wait_complete(10)
+
+        comm.home(-1)  # Start homing Z-axis
+        comm.wait_complete(60)
+
+        STATUS.connect('mode-manual', lambda w: self.on_mode_manual())
+        STATUS.connect('mode-mdi', lambda w: self.on_mode_mdi())
+        STATUS.connect('mode-auto', lambda w: self.on_mode_auto())
+
+        comm.mode(linuxcnc.MODE_AUTO)
+
+        # Set up timer to update current mode every 200ms
+        self.mode_timer = QtCore.QTimer()
+        self.mode_timer.timeout.connect(self.timer_function)
+        self.mode_timer.start(200)
+
+        self.w.statuslabel_2.setText(f"STARTED")  
 
     def processed_key_event__(self,receiver,event,is_pressed,key,code,shift,cntrl):
         # when typing in MDI, we don't want keybinding to call functions
@@ -113,9 +147,72 @@ class HandlerClass:
     # CALLBACKS FROM FORM #
     #######################
 
+    def tabChange(self, value):
+        if value == 0:
+            comm.mode(linuxcnc.MODE_AUTO)
+        elif value == 2:
+            comm.mode(linuxcnc.MODE_MANUAL)
+        elif value == 3:
+            comm.mode(linuxcnc.MODE_MDI)
+
+    def gcodeEdit (self):
+
+        self.w.stackedWidget.setCurrentIndex(1)
+
+        current_dir = self.w.filemanager.getCurrentSelected()
+
+        file = current_dir[0]
+        
+        # self.w.gcodeeditor.editMode()
+        self.w.gcodeeditor.editor.setReadOnly(False)
+        self.w.gcodeeditor.editor.load_text(file)
+
+    def gcodeRead (self):
+
+        self.w.stackedWidget.setCurrentIndex(0)
+
+        current_dir = self.w.filemanager.getCurrentSelected()
+
+        file = current_dir[0]
+
+        saved = ACTION.SAVE_PROGRAM(self.w.gcodeeditor.editor.text(), file)
+        if saved is not None:
+            LOG.debug(f"File Saved: {file}")
+
+            # self.w.gcodeeditor.readMode()
+            self.w.gcodeeditor.editor.setReadOnly(True)
+
     #####################
     # GENERAL FUNCTIONS #
     #####################
+
+    # Update current mode display every 200ms
+    def timer_function(self):
+        
+        if STATUS.is_auto_running():
+            self.w.statuslabel_2.setText(f"PROGRAM RUNNING") 
+
+            self.w.tabwidget.setTabEnabled(1, False)  # EDIT
+            self.w.tabwidget.setTabEnabled(2, False)  # JOG
+            self.w.tabwidget.setTabEnabled(3, False)  # MDI
+
+        elif STATUS.is_auto_paused():
+            self.w.statuslabel_2.setText(f"PROGRAM PAUSED")
+        else:
+            self.w.statuslabel_2.setText(f"IDLE")
+            
+            self.w.tabwidget.setTabEnabled(1, True)  # EDIT
+            self.w.tabwidget.setTabEnabled(2, True)  # JOG
+            self.w.tabwidget.setTabEnabled(3, True)  # MDI
+
+    def on_mode_manual(self):
+        self.w.statuslabel.setText(f"JOG") 
+
+    def on_mode_auto(self):
+        self.w.statuslabel.setText(f"AUTO") 
+
+    def on_mode_mdi(self):
+        self.w.statuslabel.setText(f"MDI") 
 
     # keyboard jogging from key binding calls
     # double the rate if fast is true
@@ -189,6 +286,11 @@ class HandlerClass:
     ###########################
     # **** closing event **** #
     ###########################
+
+    def closing_cleanup__(self):
+        # Stop the mode update timer
+        if hasattr(self, 'mode_timer'):
+            self.mode_timer.stop()
 
     ##############################
     # required class boiler code #
